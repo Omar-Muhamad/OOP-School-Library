8b5a528a026f06e5587a39f4a87f427d7c605c26
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-03-06 00:25:15 +0200

    Refactor App class

--- app.rb
+++ app.rb
@@ -22,33 +22,26 @@ class App
     puts '[6] list all rentals'
     puts '[7] Exit'
     puts
-    print 'choose number of operation you want to proceed: '
+    print 'Choose number of operation you want to proceed: '
     user_choice = gets.chomp
     operator(user_choice)
   end
 
   def operator(user_choice)
     case user_choice
-    when '1'
-      list_books
-    when '2'
-      list_people
-    when '3'
-      create_person
-    when '4'
-      create_book
-    when '5'
-      create_rental
-    when '6'
-      list_rentals
-    else
-      exit
+    when '1' then list_books
+    when '2' then list_people
+    when '3' then create_person
+    when '4' then create_book
+    when '5' then create_rental
+    when '6' then list_rentals
+    else exit
     end
   end
 
   def list_books
     if @books_list.length.zero?
-      p 'There is no books to show, Add new one'
+      puts '>>>>>>>>>>>>>>>>>  There is no books to show, Add new one  <<<<<<<<<<<<<<<<<'
     else
       @books_list.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
     end
@@ -57,17 +50,15 @@ class App
 
   def list_people
     if @people_list.length.zero?
-      p 'There is no people to show, Add new one'
+      puts '>>>>>>>>>>>>>>>>>  There is no people to show, Add new one  <<<<<<<<<<<<<<<<<'
     else
-      @people_list.each do |person|
-        p "Person ID: #{person.id}, Name: #{person.name} (#{person.class.name}), Age: #{person.age}"
-      end
+      @people_list.each { |person| puts "Person ID: #{person.id}, Name: #{person.name} (#{person.class.name}), Age: #{person.age}" }
     end
     run_program
   end
 
   def create_person
-    p 'What do you want to create?'
+    puts '>>>>>>>>>>>>>>>>>  What do you want to create?  <<<<<<<<<<<<<<<<<'
     puts '[1] Student'
     puts '[2] Teacher'
     print 'Enter number: '
@@ -75,23 +66,24 @@ class App
     case choice
     when '1' then add_student
     when '2' then add_teacher
-    else p 'Please enter a valid input, it must be the number 1 or the number 2'
+    else
+      puts '>>>>>>>>>>>>>>>>>  Please enter a valid input, 1 or 2  <<<<<<<<<<<<<<<<<'
     end
+    run_program
   end
 
   def add_student
-    print 'Student classroom: '
+    print 'Enter Student classroom: '
     classroom = gets.chomp.to_i
-    print 'Student name: '
+    print 'Enter Student name: '
     name = gets.chomp
-    print 'Student age: '
+    print 'Enter Student age: '
     age = gets.chomp.to_i
     print 'Has Parent permission? [Y/N]: '
     input = gets.chomp.downcase
     valid_permission = input == 'y'
     @people_list.push(Student.new(classroom, age, name, parent_permission: valid_permission))
-    p 'Student added successfully'
-    run_program
+    puts '>>>>>>>>>>>>>>>>>  Student added successfully  <<<<<<<<<<<<<<<<<'
   end
 
   def add_teacher
@@ -102,8 +94,7 @@ class App
     print 'Teacher specialization: '
     specialization = gets.chomp
     @people_list.push(Teacher.new(specialization, age, name))
-    p 'Teacher added successfully'
-    run_program
+    puts '>>>>>>>>>>>>>>>>>  Teacher added successfully  <<<<<<<<<<<<<<<<<'
   end
 
   def create_book
@@ -112,55 +103,51 @@ class App
     print 'Book author: '
     author = gets.chomp
     @books_list.push(Book.new(title, author))
-    p 'Book added successfully'
+    puts '>>>>>>>>>>>>>>>>>  Book added successfully  <<<<<<<<<<<<<<<<<'
     run_program
   end
 
   def create_rental
-    p 'Please select a book: '
-    @books_list.each_with_index do |book, id|
-      puts "[#{id}] #{book.title}. By: #{book.author}"
+    if @books_list.length.zero?
+      puts '>>>>>>>>>>>>>>>>>  No books found, add one first  <<<<<<<<<<<<<<<<<'
+    elsif @people_list.length.zero?
+      puts '>>>>>>>>>>>>>>>>>  No people found, add one first  <<<<<<<<<<<<<<<<<'
+    else
+      puts '>>>>>>>>>>>>>>>>>  Please select a book  <<<<<<<<<<<<<<<<<'
+      @books_list.each_with_index { |book, id| puts "[#{id}] #{book.title}. By: #{book.author}" }
+      print 'Choose book number: '
+      book = @books_list[gets.chomp.to_i]
+      puts '>>>>>>>>>>>>>>>>>  Please select the person  <<<<<<<<<<<<<<<<<'
+      @people_list.each_with_index { |person, id| puts "[#{id}] Name: #{person.name} (#{person.class.name})" }
+      print 'Choose person number: '
+      person = @people_list[gets.chomp.to_i]
+      print 'Date of rental (dd-mm-yyyy): '
+      date = gets.chomp
+      @rentals_list.push(Rental.new(date, book, person))
+      puts '>>>>>>>>>>>>>>>>>  Rental data added successfully  <<<<<<<<<<<<<<<<<'
     end
-    print 'Choose book number: '
-    book = @books_list[gets.chomp.to_i]
-    p 'Please select the person'
-    @people_list.each_with_index do |person, id|
-      puts "[#{id}] Name: #{person.name} (#{person.class.name})"
-    end
-    print 'Choose person number: '
-    person = @people_list[gets.chomp.to_i]
-    print 'Date of rental (dd-mm-yyyy): '
-    date = gets.chomp
-    @rentals_list.push(Rental.new(date, book, person))
-    p 'Rental data added successfully'
     run_program
   end
 
   def list_rentals
-    puts
     if @rentals_list.length.zero?
-      p 'There is no rentals to show, Add new one'
-      puts
-      run_program
+      puts '>>>>>>>>>>>>>>>>>  There is no rentals to show, Add new one  <<<<<<<<<<<<<<<<<'
     else
       print "Enter person's id: "
       id = gets.chomp.to_i
       @people_list.each do |person|
         if person.id == id
-          person.rentals.each do |rental|
-            p "Book title: #{rental.book.title}, Rental date: #{rental.date}"
-          end
-          run_program
+          person.rentals.each { |rental| puts "Book title: #{rental.book.title}, Rental date: #{rental.date}" }
+          break
         else
-          puts
-          p 'There are no rentals data for that id'
+          puts '>>>>>>>>>>>>>>>>>  There are no rentals data for that id  <<<<<<<<<<<<<<<<<'
         end
       end
     end
+    run_program
   end
 
   def exit
-    puts
-    p 'Have a great day, Bye...'
+    puts '>>>>>>>>>>>>>>>>>  Have a great day, Bye...  <<<<<<<<<<<<<<<<<'
   end
 end
