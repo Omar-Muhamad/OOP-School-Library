8964ec7ce2f658570460f80faa439620ba021dc6
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-03-09 22:46:38 +0200

    Add tests to Person class

---
+++ tests/person_spec.rb
@@ -0,0 +1,15 @@
+require '../classes/person'
+
+describe 'Create person object' do
+  context 'given the valid inputs' do
+    it 'should create a person with age = 10' do
+      person = Person.new(20)
+      expect(person.age).to eql(20)
+    end
+    it 'should create a person with age = 20 and called Omar' do
+      person = Person.new(20, 'Omar')
+      expect(person.age).to eql(20)
+      expect(person.name).to match('Omar')
+    end
+  end
+end
