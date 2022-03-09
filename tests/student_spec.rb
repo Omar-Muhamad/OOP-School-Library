8c4034ab44ddbd867d051324b55f92ced1c7acea
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-03-09 23:03:19 +0200

    Add unit test to Student class

---
+++ tests/student_spec.rb
@@ -0,0 +1,17 @@
+require '../classes/student'
+
+describe 'Create student object' do
+  context 'given the valid inputs' do
+    it 'should create a student with classroom = 10 and age = 16' do
+      student = Student.new(10, 16)
+      expect(student.classroom).to eql(10)
+      expect(student.age).to eql(16)
+    end
+    it 'should create a student with classroom = 10, age = 16, and name = Omar ' do
+      student = Student.new(10, 16, 'Omar')
+      expect(student.classroom).to eql(10)
+      expect(student.age).to eql(16)
+      expect(student.name).to eql('Omar')
+    end
+  end
+end
