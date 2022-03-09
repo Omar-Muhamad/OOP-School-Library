d18c9a57d51625f18d3be6e3f6e2897defcbdf73
kerolous samy <keroloussamy98@gmail.com>
2022-03-09 23:27:21 +0200

    Fix linters

--- tests/rental_spec.rb
+++ tests/rental_spec.rb
@@ -3,15 +3,13 @@ require '../classes/person'
 require '../classes/book'
 
 describe Rental do
-  context "When testing the Rental class" do
-
-    it "The initialize method should return create new Rental object" do
+  context 'When testing the Rental class' do
+    it 'The initialize method should return create new Rental object' do
       book = Book.new('Harry Potter', 'J.K. Rowling')
       person = Person.new(20)
-      date = "2222/2/2"
+      date = '2222/2/2'
       rental = Rental.new(date, book, person)
-      expect(rental.date).to eq "2222/2/2"
+      expect(rental.date).to eq '2222/2/2'
     end
-
-  end 
+  end
 end
--- tests/teacher_spec.rb
+++ tests/teacher_spec.rb
@@ -1,16 +1,15 @@
 require '../classes/teacher'
 
 describe Teacher do
-  context "When testing the Teacher class" do
-
-    it "The initialize method should return create new Teacher object" do
+  context 'When testing the Teacher class' do
+    it 'The initialize method should return create new Teacher object' do
       teacher = Teacher.new('Math', 20, 'Kerolous')
-      expect(teacher.name).to eq "Kerolous"
+      expect(teacher.name).to eq 'Kerolous'
     end
-     
-    it "The can_use_services? method should return true" do
+
+    it 'The can_use_services? method should return true' do
       teacher = Teacher.new('Math', 20, 'Kerolous')
       expect(teacher.can_use_services?).to be true
     end
-  end 
+  end
 end
